d04ee2f7dcd09d7fd48645dbe50cda6928bf16a7
Nico <nimustafa03@gmail.com>
2025-10-15 16:02:46 -0300

    Delete 00-ChelloWorld directory
    Inicio Corrección

--- 00-ChelloWorld/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-FILE *archivo; 
-
-int main(){
-    archivo = freopen ("output.txt", "w",stdout);
-    fprintf(stdout, "Hola Nicolás Samir Mustafa!");
-    fclose (archivo);
-    return 0;
-}
