0dfc715424b638fce0fbe6f6686a37c3de6ff1b4
Nico <nimustafa03@gmail.com>
2025-10-22 15:11:11 -0300

    Eliminada escritura a archivo. Cambiado fputs por printf.

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include <conio.h>
 
-
-FILE* archivo;
 const char *msg = "Hola, Nicolas Samir Mustafa!\n";
 
 int main() {
-    
-    archivo = fopen("output.txt", "w");
-
-    fputs(msg,stdout);
-    fputs(msg,archivo);
-    fflush(stdout);
-    fclose(archivo);
+    stdout;
+    printf(msg);
     getchar();
     return 0;
-
 }
